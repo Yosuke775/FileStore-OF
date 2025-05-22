b620bb26d13fa85f0ea4b26563dd712de793bffa
Yosuke775 <priyanshuprasad775@gmail.com>
2025-05-22 22:04:41 +0530

    Update config.py

--- config.py
+++ config.py
@@ -36,8 +36,8 @@ FSUB_LINK_EXPIRY = int(os.getenv("FSUB_LINK_EXPIRY", "10"))  # 0 means no expiry
 BAN_SUPPORT = os.environ.get("BAN_SUPPORT", "https://t.me/+qAwnfPIm6Wo2ODJl")
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "200"))
 #--------------------------------------------
-START_PIC = os.environ.get("START_PIC", "https://www.hdwallpapers.in/download/roronoa_zoro_4k_hd_one_piece-HD.jpg")
-FORCE_PIC = os.environ.get("FORCE_PIC", "https://graph.org/file/7bcf32b2c965ea96a6600-7ec885c41bba422e7b.jpg")
+START_PIC = os.environ.get("START_PIC", "https://graph.org/file/ad59c07cc2ae60f469786-e5ed6b2d233d25e1b4.jpg")
+FORCE_PIC = os.environ.get("FORCE_PIC", "https://graph.org/file/ad59c07cc2ae60f469786-e5ed6b2d233d25e1b4.jpg")
 #--------------------------------------------
 
 #--------------------------------------------
